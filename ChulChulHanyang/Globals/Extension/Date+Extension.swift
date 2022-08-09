c160dd3dc634073f5d7cff96dc7bf147eb3afd82
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-08-09 22:17:12 +0900

    [Feat[ Date extension으로 년,월,일 리턴하는 변수 추가

--- ChulChulHanyang/Globals/Extension/Date+Extension.swift
+++ ChulChulHanyang/Globals/Extension/Date+Extension.swift
@@ -17,4 +17,16 @@ extension Date {
         return DateFormatterLiteral.dayDateFormatter.string(from: self)
     }
     
+    var year: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    var month: Int {
+        return Calendar.current.component(.month, from: self)
+    }
+    
+    var day: Int {
+        return Calendar.current.component(.day, from: self)
+    }
+    
 }
