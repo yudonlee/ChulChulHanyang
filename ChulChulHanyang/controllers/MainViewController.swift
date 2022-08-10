baf3a0d74d9f5ff0b8451c1e32e4125e4ac7a4e6
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-08-10 17:36:27 +0900

    [Feat] 식당정보가 없을때 Label띄우기

--- ChulChulHanyang/controllers/MainViewController.swift
+++ ChulChulHanyang/controllers/MainViewController.swift
@@ -23,6 +23,15 @@ final class MainViewController: UIViewController {
         return collectionView
     }()
     
+    private let emptyMenuInformation: UILabel = {
+       let label = UILabel()
+        label.text = "해당 식당은 오늘 운영하지 않아요"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        label.isHidden = true
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         render()
@@ -46,13 +55,14 @@ final class MainViewController: UIViewController {
         })
         DispatchQueue.main.async { [weak self] in
             self?.dietCollectionView.reloadData()
+            self?.emptyMenuInformation.isHidden = (self?.data.isEmpty)! ? false : true
         }
     }
     
     private func render() {
         self.view.backgroundColor = .backgroundGray
         
-        view.addSubviews(datePartView, restaurantSelectView, dietCollectionView)
+        view.addSubviews(datePartView, restaurantSelectView, dietCollectionView, emptyMenuInformation)
         
         let datePartViewConstraints = [
             datePartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
@@ -76,7 +86,15 @@ final class MainViewController: UIViewController {
             
         ]
         
-        [datePartViewConstraints, restaurantSelectViewConstraints, dietCollectionViewConstraints].forEach { constraint in
+        let emptyMenuInformationConstraints = [
+            emptyMenuInformation.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            emptyMenuInformation.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            emptyMenuInformation.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            emptyMenuInformation.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            emptyMenuInformation.heightAnchor.constraint(equalToConstant: 30)
+        ]
+        
+        [datePartViewConstraints, restaurantSelectViewConstraints, dietCollectionViewConstraints, emptyMenuInformationConstraints].forEach { constraint in
             NSLayoutConstraint.activate(constraint)
         }
     }
@@ -93,7 +111,8 @@ extension MainViewController: UICollectionViewDataSource {
         }
         
         cell.layer.cornerRadius = 22
-        cell.configure(with: data[indexPath.row])
+        let model = MenuViewModel(diet: data[indexPath.row], type: restaurantSelectView.typeData())
+        cell.configure(with: model)
         
         return cell
     }
@@ -103,7 +122,7 @@ extension MainViewController: UICollectionViewDataSource {
 
 extension MainViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 345, height: data[indexPath.row].count * 40 + 20)
+        return CGSize(width: 345, height: data[indexPath.row].count * 40 + 20 + 10)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
