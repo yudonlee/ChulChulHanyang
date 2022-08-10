e6644269c43f59d9fdede3f15d4e1a0c453ddd82
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-08-10 17:34:54 +0900

    [Feat] 메뉴 전달시, 식당 데이터 타입도 받도록 구현

---
+++ ChulChulHanyang/ViewModels/MenuViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewModel.swift
+//  ChulChulHanyang
+//
+//  Created by yudonlee on 2022/08/09.
+//
+
+import Foundation
+
+struct MenuViewModel {
+    let diet: [String]
+    let type: RestaurantType
+}
