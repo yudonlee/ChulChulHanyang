ed9a980f9bb6f51549a357e2c80b3b0098d75c77
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-08-12 11:02:40 +0900

    [Feat] Alamofire import 삭제(빌드 가볍게)

--- ChulChulHanyang/Globals/Manager/CrawlManager.swift
+++ ChulChulHanyang/Globals/Manager/CrawlManager.swift
@@ -5,7 +5,6 @@
 //  Created by yudonlee on 2022/08/08.
 //
 
-import Alamofire
 import SwiftSoup
 import Foundation
 
@@ -15,7 +14,7 @@ final class CrawlManager {
     
     private init() { }
     
-    let ramenInformation: [String] = ["분식(14:00 ~ 15:00)", "라면", "2000원", "떡 or 만두 or 치즈 라면", "2300원", "떡 or 만두 or 치즈 라면 + 공깃밥", "2800원"]
+    let ramenInformation: [String] = ["분식", "라면", "2000원", "떡 or 만두 or 치즈 라면", "2300원", "떡 or 만두 or 치즈 라면 + 공깃밥", "2800원"]
     
     private func getRestaurantURL(type: Int, month: Int, day: Int, year: Int) -> String {
         let str = "\(URLConstants.baseURL)\(type)\(URLConstants.dateURL)\(day)\(URLConstants.yearURL) \(year)\(URLConstants.monthURL)\(month - 1)"
@@ -60,7 +59,6 @@ final class CrawlManager {
     func crawlHanyangPlaza(url: URL) -> [[String]]? {
         
         var result = [[String]]()
-//        result.append(ramenInformation)
         
         do {
             let html = try String(contentsOf: url)
@@ -70,26 +68,42 @@ final class CrawlManager {
             try inbox.forEach { element in
                 
                 
-                var output = try element.text()
-//                if output.contains("라면") { return }
+                var str = try element.text()
+                if str.contains("라면") {
+                    result.append(ramenInformation)
+                    return
+                }
                 
-                var str = output.replacingOccurrences(of: "[a-zA-Z]", with: "", options: .regularExpression)
+//                학생식당 영어 번역 메뉴 삭제
+                str = str.replacingOccurrences(of: "[a-zA-Z]", with: "", options: .regularExpression)
                 str.removeAll(where: { [","].contains($0) })
                 var convertedStrArray = str.components(separatedBy: " ").map { String($0) }.filter { element in
-                    return !element.isEmpty
+                    return !element.isEmpty && !(element.contains(":"))
                 }
                 
                 if convertedStrArray.contains("공통찬") {
                     let firstIdx = convertedStrArray.lastIndex(of: "공통찬")
                     let lastIdx = convertedStrArray.firstIndex(of: "*")
                     guard let firstIdx = firstIdx, let lastIdx = lastIdx else {
-                            return
+                        return
                     }
-                    let arr = convertedStrArray[firstIdx...lastIdx - 1].map({ String($0) })
-                    result.append(arr)
-                } else {
-                    result.append(convertedStrArray)
+                    convertedStrArray = convertedStrArray[firstIdx...lastIdx - 1].map({ String($0) })
                 }
+                
+//                if convertedStrArray.contains("라면") {
+//                    convertedStrArray.lastIndex(of: "+")
+//                    
+//                    
+//                    let firstRiceCake = convertedStrArray.firstIndex(of: "떡")
+//                    let lastRiceCake = convertedStrArray.firstIndex(of: "치즈")
+//                    
+//                    let firstCheese = convertedStrArray.firstIndex(of: "치즈")
+//                    let lastCheese = convertedStrArray.lastIndex(of: "치즈")
+//                    
+//                    convertedStrArray[]
+//                }
+                
+                result.append(convertedStrArray)
             }
         } catch Exception.Error(let type, let message) {
             print("Message: \(message)")
