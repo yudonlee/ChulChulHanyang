fb27ac72620cfb2f775f6fe0d10679580c4ed725
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-08-29 21:21:55 +0900

    [FEAT] 위젯 파싱 매니저, 비동기 처리

--- ChulChulHanyangWidget/Manager/ParsingManager.swift
+++ ChulChulHanyangWidget/Manager/ParsingManager.swift
@@ -31,6 +31,270 @@ struct ParsingManager {
         return "중식"
     }
     
+    static private func isUserDefaultDataToday(type: RestaurantType) -> Bool {
+        let dateText: String = {
+            let date = Date()
+            return date.keyText
+        }()
+        
+        if UserDefaults.standard.string(forKey: "DateOf\(type.name)") == "\(dateText)" {
+            return true
+        }
+        return false
+    }
+    
+    static private func shouldUserDefaultUpdate(type: RestaurantType) -> Bool {
+        
+        let dateText: String = {
+            let date = Date()
+            return date.keyText
+        }()
+        
+        
+        guard let data = UserDefaults.standard.string(forKey: "DateOf\(type.name)") else {
+            return true
+        }
+        
+        if data != "\(dateText)" {
+            return true
+        }
+        
+        return false
+        
+    }
+    
+    
+    static func parsingAsync(type: RestaurantType, completion: @escaping (Result<[String], Error>) -> Void) {
+        
+        CrawlManager.shared.crawlRestaurantMenuAsyncAndURL(date: Date(), restaurantType: type) { result in
+            switch result {
+            case .success(let data):
+                var parsedData = [String]()
+                
+                let getTimeStamp = getMealTime(type: type)
+                
+                switch type {
+                case .HanyangPlaza:
+                    parsedData = data.filter { str in
+                        str.contains("중식/석식")
+                    }.flatMap({ $0 })
+                    
+                case .HumanEcology:
+                    parsedData = data.filter { str in
+                        str.contains(getTimeStamp)
+                    }.flatMap({ $0 })
+                    
+                    if let damAIndex = parsedData.firstIndex(of: "[Dam-A]"),
+                       let pangeosIndex = parsedData.firstIndex(of: "[Pangeos]") {
+                        
+                        let range = pangeosIndex..<parsedData.endIndex
+                        parsedData.removeSubrange(range)
+                    }
+                    
+                    
+                case .MaterialScience:
+                    parsedData = data.filter { str in
+                        str.contains(getTimeStamp)
+                    }.flatMap({ $0 })
+                    
+                    if let jeongsikIndex = parsedData.firstIndex(of: "[정식]"),
+                       let ilpoomIndex = parsedData.firstIndex(of: "[일품]") {
+                        
+                        let range = ilpoomIndex..<parsedData.endIndex
+                        parsedData.removeSubrange(range)
+                    }
+                    
+                case .ResidenceOne, .ResidenceTwo:
+                    parsedData = data.filter { str in
+                        str.contains(getTimeStamp)
+                    }.flatMap({ $0 })
+                    
+                    
+                case .HangwonPark:
+                    var result = data.filter { str in
+                        str.contains(getTimeStamp)
+                    }.flatMap({ $0 })
+                    
+                    if let cornerAIndex = result.firstIndex(of: "[코너 A]"),
+                       let cornerBIndex = result.firstIndex(of: "[코너 B]") {
+                        
+                        let range = cornerBIndex..<result.endIndex
+                        result.removeSubrange(range)
+                    }
+                    
+                }
+                
+                parsedData = parsedData.filter { str in
+                    return str != "-"
+                }
+                completion(.success(parsedData))
+                
+            case .failure(let error):
+                completion(.failure(error))
+                
+            }
+        }
+    }
+    
+    
+    static func parsingAsyncAndLocal(type: RestaurantType, completion: @escaping (Result<[String], Error>) -> Void) {
+        
+        let dateText: String = {
+            let date = Date()
+            return date.keyText
+        }()
+//        if false {
+        print(UserDefaults.standard.array(forKey: "TodayMenuOf\(type.name)"))
+        if isUserDefaultDataToday(type: type), let data = UserDefaults.standard.array(forKey: "TodayMenuOf\(type.name)") as? [[String]] {
+//            var data: [[String]] = []
+            var parsedData = [String]()
+            
+            let getTimeStamp = getMealTime(type: type)
+            
+            switch type {
+            case .HanyangPlaza:
+                parsedData = data.filter { str in
+                    str.contains("중식/석식")
+                }.flatMap({ $0 })
+                
+            case .HumanEcology:
+                parsedData = data.filter { str in
+                    str.contains(getTimeStamp)
+                }.flatMap({ $0 })
+                
+                if let damAIndex = parsedData.firstIndex(of: "[Dam-A]"),
+                   let pangeosIndex = parsedData.firstIndex(of: "[Pangeos]") {
+                    
+                    let range = pangeosIndex..<parsedData.endIndex
+                    parsedData.removeSubrange(range)
+                }
+                
+                
+            case .MaterialScience:
+                parsedData = data.filter { str in
+                    str.contains(getTimeStamp)
+                }.flatMap({ $0 })
+                
+                if let jeongsikIndex = parsedData.firstIndex(of: "[정식]"),
+                   let ilpoomIndex = parsedData.firstIndex(of: "[일품]") {
+                    
+                    let range = ilpoomIndex..<parsedData.endIndex
+                    parsedData.removeSubrange(range)
+                }
+                
+            case .ResidenceOne, .ResidenceTwo:
+                parsedData = data.filter { str in
+                    str.contains(getTimeStamp)
+                }.flatMap({ $0 })
+                
+            case .HangwonPark:
+                var result = data.filter { str in
+                    str.contains(getTimeStamp)
+                }.flatMap({ $0 })
+                
+                if let cornerAIndex = result.firstIndex(of: "[코너 A]"),
+                   let cornerBIndex = result.firstIndex(of: "[코너 B]") {
+                    
+                    let range = cornerBIndex..<result.endIndex
+                    result.removeSubrange(range)
+                }
+                
+            }
+            
+            parsedData = parsedData.filter { str in
+                return str != "-"
+            }
+            completion(.success(parsedData))
+        }
+        
+        else {
+            CrawlManager.shared.crawlRestaurantMenuAsyncAndURL(date: Date(), restaurantType: type) { result in
+                switch result {
+                case .success(let data):
+                    let parsed = data.map({ strArray in
+                        strArray.filter { str in
+                            !["-"].contains(str)
+                        }
+                    })
+                    
+                    if shouldUserDefaultUpdate(type: type) {
+                        UserDefaults.standard.set("\(dateText)", forKey: "DateOf\(type.name)")
+                        print(UserDefaults.standard.string(forKey: "DateOf\(type.name)"))
+                        UserDefaults.standard.set(parsed, forKey: "TodayMenuOf\(type.name)")
+                        print(UserDefaults.standard.array(forKey: "TodayMenuOf\(type.name)"))
+                    }
+                    
+                    var parsedData = [String]()
+                    
+                    let getTimeStamp = getMealTime(type: type)
+                    
+                    switch type {
+                    case .HanyangPlaza:
+                        parsedData = data.filter { str in
+                            str.contains("중식/석식")
+                        }.flatMap({ $0 })
+                        
+                    case .HumanEcology:
+                        parsedData = data.filter { str in
+                            str.contains(getTimeStamp)
+                        }.flatMap({ $0 })
+                        
+                        if let damAIndex = parsedData.firstIndex(of: "[Dam-A]"),
+                           let pangeosIndex = parsedData.firstIndex(of: "[Pangeos]") {
+                            
+                            let range = pangeosIndex..<parsedData.endIndex
+                            parsedData.removeSubrange(range)
+                        }
+                        
+                        
+                    case .MaterialScience:
+                        parsedData = data.filter { str in
+                            str.contains(getTimeStamp)
+                        }.flatMap({ $0 })
+                        
+                        if let jeongsikIndex = parsedData.firstIndex(of: "[정식]"),
+                           let ilpoomIndex = parsedData.firstIndex(of: "[일품]") {
+                            
+                            let range = ilpoomIndex..<parsedData.endIndex
+                            parsedData.removeSubrange(range)
+                        }
+                        
+                    case .ResidenceOne, .ResidenceTwo:
+                        parsedData = data.filter { str in
+                            str.contains(getTimeStamp)
+                        }.flatMap({ $0 })
+                    case .HangwonPark:
+                        var result = data.filter { str in
+                            str.contains(getTimeStamp)
+                        }.flatMap({ $0 })
+                        
+                        if let cornerAIndex = result.firstIndex(of: "[코너 A]"),
+                           let cornerBIndex = result.firstIndex(of: "[코너 B]") {
+                            
+                            let range = cornerBIndex..<result.endIndex
+                            result.removeSubrange(range)
+                        }
+                        
+                    }
+                    
+                    parsedData = parsedData.filter { str in
+                        return str != "-"
+                    }
+                    completion(.success(parsedData))
+                    
+                case .failure(let error):
+                    completion(.failure(error))
+                    
+                }
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    
     static func parsing(type: RestaurantType) -> [String] {
         guard let data = CrawlManager.shared.crawlRestaurantMenu(date: Date(), restaurantType: type) else {
             return [String]()
@@ -45,7 +309,7 @@ struct ParsingManager {
             result = data.filter { str in
                 str.contains("중식/석식")
             }.flatMap({ $0 })
-        
+            
         case .HumanEcology:
             result = data.filter { str in
                 str.contains(getTimeStamp)
@@ -94,4 +358,5 @@ struct ParsingManager {
         }
         return result
     }
+    
 }
