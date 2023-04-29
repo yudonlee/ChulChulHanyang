ed4a2f8245a213ffeae3bde21d0485a7469d26ee
yudonlee <39371835+yudonlee@users.noreply.github.com>
2023-04-30 00:05:27 +0900

    [Feat] 메인뷰 리빌딩, StackView로 변경

--- ChulChulHanyang/controllers/MainViewController.swift
+++ ChulChulHanyang/controllers/MainViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
-    lazy private var data: [[String]] = [[String]]()
+    
+    enum Section {
+        case main
+    }
+    
+    struct Item: Hashable {
+        private let identifier = UUID()
+        let restaurantType: RestaurantType
+        
+        init(restaurantType: RestaurantType) {
+            self.restaurantType = restaurantType
+        }
+    }
     
     lazy private var datePartView: DateView = {
         let dateView = DateView()
@@ -17,155 +29,71 @@ final class MainViewController: UIViewController {
         return dateView
     }()
     
-    lazy private var restaurantSelectView: RestaurantListView = {
-        let view = RestaurantListView()
-        view.delegate = self
-        return view
+    private let segmentScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        return scrollView
+    }()
+    
+    lazy private var segmentCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .backgroundGray
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
+    private let segmentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        return stackView
     }()
     
     private var type: RestaurantType = .HumanEcology
     private var date: Date = Date()
+    private var restaurantMenuListViews: [RestaurantMenuListView]!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
-    
-    lazy private var dietCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(DietCollectionViewCell.self, forCellWithReuseIdentifier: DietCollectionViewCell.identifier)
-        collectionView.backgroundColor = .backgroundGray
-        collectionView.allowsSelection = false
-        collectionView.refreshControl = UIRefreshControl()
-        collectionView.refreshControl?.addTarget(self, action: #selector(requestDataByRefreshControl), for: .valueChanged)
-        return collectionView
-    }()
-    
-    private let emptyMenuInformation: UILabel = {
-       let label = UILabel()
-        label.text = "등록된 정보를 찾지 못했어요😢\n 화면을 아래로 당겨 새로고침 해보세요."
-        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
-        label.isHidden = true
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        return label
-    }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        render()
-        dietCollectionView.delegate = self
-        dietCollectionView.dataSource = self
-        requestData()
+        
+        setSegmentViews()
+        
+        configureLayout()
+        configureDataSource()
+        
+        segmentCollectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: true, scrollPosition: .centeredHorizontally)
     }
     
+    private func setSegmentViews() {
+        restaurantMenuListViews = RestaurantType.allCases.map {
+            let view = RestaurantMenuListView()
+            view.setViewModel(viewModel: RestaurantMenuListViewModel(restaurant: $0))
+            return view
+        }
+    }
+    
+    // Process Deeplink
     func selectRestaurant(restaurant: String) {
         guard let restaurant = RestaurantType(englishName: restaurant) else { return }
         if restaurant != self.type {
-            restaurantSelectView.selectRestaurant(restaurant: restaurant)
+            // TODO: 레스토랑 변경
+//            restaurantSelectView.selectRestaurant(restaurant: restaurant)
         }
     }
-    
-    private func isUserDefaultDataToday() -> Bool {
-        if Calendar.current.isDateInToday(date), UserDefaults.shared.string(forKey: "DateOf\(type.name)") == "\(date.keyText)" {
-            return true
-        }
-        return false
-    }
-    
-    private func shouldUserDefaultUpdate() -> Bool {
-        
-        let dateText: String = {
-            let date = Date()
-            return date.keyText
-        }()
-        
-        if Calendar.current.isDateInToday(date) {
-            guard let data = UserDefaults.shared.string(forKey: "DateOf\(type.name)") else {
-                return true
-            }
-            
-            if data != "\(dateText)" {
-                return true
-            }
-        }
-        return false
-        
-    }
-    
-    @objc
-    private func requestDataByRefreshControl() {
-        CrawlManager.shared.crawlRestaurantMenuAsyncAndURL(date: date,  restaurantType: type, completion: { [weak self] result in
-            switch result {
-            case .success(let crawledData):
-                let parsed = crawledData.map({ strArray in
-                    strArray.filter { str in
-                        !["-"].contains(str)
-                    }
-                })
-                
-                if !parsed.isEmpty, let status = self?.shouldUserDefaultUpdate(), status, let dateKeyText = self?.date.keyText, let typeName = self?.type.name {
-                    UserDefaults.shared.set("\(dateKeyText)", forKey: "DateOf\(typeName)")
-                    UserDefaults.shared.set(parsed, forKey: "TodayMenuOf\(typeName)")
-                }
-                
-                DispatchQueue.main.async { [weak self] in
-                    self?.data = parsed
-                    self?.dietCollectionView.reloadData()
-                    self?.emptyMenuInformation.isHidden = (self?.data.isEmpty)! ? false : true
-                    self?.dietCollectionView.refreshControl?.endRefreshing()
-                }
-                
-            case .failure(let error):
-                print(error.localizedDescription)
-                DispatchQueue.main.async {
-                    self?.dietCollectionView.refreshControl?.endRefreshing()
-                }
-            }
-        })
-    }
-    
-    func requestData() {
-        
-        if isUserDefaultDataToday(), let data = UserDefaults.shared.array(forKey: "TodayMenuOf\(type.name)") as? [[String]] {
-            DispatchQueue.main.async { [weak self] in
-                self?.data = data
-                self?.dietCollectionView.reloadData()
-                self?.emptyMenuInformation.isHidden = (self?.data.isEmpty)! ? false : true
-            }
-        } else {
-            LoadingService.showLoading()
-            CrawlManager.shared.crawlRestaurantMenuAsyncAndURL(date: date,  restaurantType: type, completion: { [self] result in
-                switch result {
-                case .success(let crawledData):
-                    let parsed = crawledData.map({ strArray in
-                        strArray.filter { str in
-                            !["-"].contains(str)
-                        }
-                    })
-                    
-                    if !parsed.isEmpty, shouldUserDefaultUpdate() {
-                        UserDefaults.shared.set("\(date.keyText)", forKey: "DateOf\(type.name)")
-                        UserDefaults.shared.set(parsed, forKey: "TodayMenuOf\(self.type.name)")
-                    }
-                    LoadingService.hideLoading()
-                    DispatchQueue.main.async { [weak self] in
-                        self?.data = parsed
-                        self?.dietCollectionView.reloadData()
-                        self?.emptyMenuInformation.isHidden = (self?.data.isEmpty)! ? false : true
-                    }
-                    
-                case .failure(let error):
-                    print(error.localizedDescription)
-                    LoadingService.hideLoading()
-                }
-            })
-        }
-    }
-    
-    
-    private func render() {
+}
+
+// MARK: Set Autolayout
+extension MainViewController {
+    private func configureLayout() {
         self.view.backgroundColor = .backgroundGray
         
-        view.addSubviews(datePartView, restaurantSelectView, dietCollectionView, emptyMenuInformation)
+        view.addSubviews(datePartView, segmentScrollView, segmentCollectionView)
+        segmentScrollView.addSubviews(segmentStackView)
+        
+        restaurantMenuListViews.forEach {
+            segmentStackView.addArrangedSubview($0)
+        }
         
         let datePartViewConstraints = [
             datePartView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
@@ -174,79 +102,84 @@ final class MainViewController: UIViewController {
             datePartView.heightAnchor.constraint(equalToConstant: 70)
         ]
         
-        let restaurantSelectViewConstraints = [
-            restaurantSelectView.topAnchor.constraint(equalTo: datePartView.bottomAnchor, constant: 24),
-            restaurantSelectView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            restaurantSelectView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            restaurantSelectView.heightAnchor.constraint(equalToConstant: 45)
+        let segmentCollectionViewConstraints = [
+            segmentCollectionView.topAnchor.constraint(equalTo: datePartView.bottomAnchor),
+            segmentCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            segmentCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            segmentCollectionView.heightAnchor.constraint(equalToConstant: 51)
         ]
         
-        let dietCollectionViewConstraints = [
-            dietCollectionView.topAnchor.constraint(equalTo: restaurantSelectView.bottomAnchor, constant: 32),
-            dietCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            dietCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            dietCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            
+        let segmentScrollViewConstraints = [
+            segmentScrollView.topAnchor.constraint(equalTo: segmentCollectionView.bottomAnchor),
+            segmentScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            segmentScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            segmentScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ]
         
-        let emptyMenuInformationConstraints = [
-            emptyMenuInformation.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            emptyMenuInformation.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            emptyMenuInformation.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            emptyMenuInformation.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        let segmentStackViewConstrinats = [
+            segmentStackView.topAnchor.constraint(equalTo: segmentScrollView.topAnchor),
+            segmentStackView.leadingAnchor.constraint(equalTo: segmentScrollView.leadingAnchor),
+            segmentStackView.trailingAnchor.constraint(equalTo: segmentScrollView.trailingAnchor),
+            segmentStackView.bottomAnchor.constraint(equalTo: segmentScrollView.bottomAnchor),
+            segmentStackView.heightAnchor.constraint(equalTo: segmentScrollView.heightAnchor)
         ]
         
-        [datePartViewConstraints, restaurantSelectViewConstraints, dietCollectionViewConstraints, emptyMenuInformationConstraints].forEach { constraint in
-            NSLayoutConstraint.activate(constraint)
-        }
-    }
-}
-
-extension MainViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.compactMap { $0 }.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DietCollectionViewCell.identifier, for: indexPath) as? DietCollectionViewCell else {
-            return UICollectionViewCell()
+        let restaurantViewConstraints = restaurantMenuListViews.map {
+            $0.widthAnchor.constraint(equalTo: segmentScrollView.widthAnchor)
         }
         
-        cell.layer.cornerRadius = 22
-        let model = MenuViewModel(diet: data[indexPath.row], type: type)
-        cell.configure(with: model)
+        [datePartViewConstraints, segmentCollectionViewConstraints, segmentScrollViewConstraints, segmentStackViewConstrinats, restaurantViewConstraints].forEach {
+                NSLayoutConstraint.activate($0)
+            }
         
-        return cell
     }
-    
-    
 }
 
-extension MainViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: Int(UIScreen.screenWidth * 0.88), height: data[indexPath.row].count * 35 + 5)
+// MARK: CollectionView Layout, DiffableDataSource {
+extension MainViewController {
+    func createLayout() -> UICollectionViewLayout {
+        let layoutSize = NSCollectionLayoutSize(widthDimension: .estimated(60), heightDimension: .absolute(31))
+        let item = NSCollectionLayoutItem(layoutSize: layoutSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+        return UICollectionViewCompositionalLayout(section: section)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 24
+    func configureDataSource() {
+        let restaurantName = ["생과대", "신소재", "제1생", "제2생", "학생식당", "행원파크"]
+        let cellRegistration = UICollectionView.CellRegistration<RestaurantCollectionViewCell, Item> { (cell, indexPath, identifier) in
+            cell.configure(with: restaurantName[indexPath.item])
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: segmentCollectionView) {
+            (collectionView, indexPath, identifier) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([Section.main])
+        snapshot.appendItems(RestaurantType.allCases.map { Item(restaurantType: $0) })
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
-
 }
 
-
-extension MainViewController: RestaurantCollectionViewCellDelegate {
-    
-    func restaurantCollectionViewCellTapped(_ type: RestaurantType) {
-        self.type = type
-        requestData()
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        // TODO: 식당 type 변경에 따른, segment 변경
+        guard let type = dataSource.itemIdentifier(for: indexPath)?.restaurantType else { return }
+        print(type)
     }
-    
 }
 
 extension MainViewController: DateViewDelegate {
     func dateViewValueChange(_ date: Date) {
         self.date = date
-        requestData()
+        // TODO: 날짜 변경에 따른 display함수 재호출
     }
     
 }
